8038b029a8b74e1d4af99be0985f8c31b6ce0c92
albmg <albmguer@gmail.com>
2020-08-05 04:06:22 +0100

    some fix

--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -1,9 +1,12 @@
 const UserModel = require('../models/users.model')
 
+const mongoose = require('mongoose')
+
+
 function getProfile (req, res) {
   UserModel
     .findById(res.locals.user._id)
-    .populate('videos')
+    .populate('productsCreated')
     .then(response => res.json(response))
     .catch(err => console.error(err))
 }
